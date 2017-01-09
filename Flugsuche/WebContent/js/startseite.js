bc35592ab43a835970f7d2485c9417bd7fa51b75
Magda0712 <magdalena.hora@gmx.de>
2017-01-09 10:35:46 +0100

    Startseite überarbeitet

--- Flugsuche/WebContent/js/startseite.js
+++ Flugsuche/WebContent/js/startseite.js
@@ -2,7 +2,7 @@ function naechstesAngebot(){
 	var aktuellesDiv = document.querySelector("article#Angebote > div.visible");
 	var angebotnummer = aktuellesDiv.className.substring(aktuellesDiv.className.indexOf("Angebot") +7);
 	var naechstenummer = 0;
-	if(angebotnummer == 3){
+	if(angebotnummer == 4){
 		naechstenummer = 1;
 	}
 	else{
@@ -18,7 +18,7 @@ function vorherigesAngebot(){
 	var angebotnummer = aktuellesDiv.className.substring(aktuellesDiv.className.indexOf("Angebot") +7);
 	var naechstenummer = 0;
 	if(angebotnummer == 1){
-		naechstenummer = 3;
+		naechstenummer = 4;
 	}
 	else{
 		naechstenummer = parseInt(angebotnummer) - 1;
